5efe50f991942861550de2f6786fc75011e9ad73
vilic <i@vilic.info>
2018-12-23 05:16:21 +0800

    Add demo content

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,15 +1,49 @@
 import 'draft-js/dist/Draft.css';
 
-import {EditorState} from 'draft-js';
+import {EditorState, convertFromRaw} from 'draft-js';
 import Editor from 'draft-js-plugins-editor';
 import React, {Component, ReactNode} from 'react';
 import ReactDOM from 'react-dom';
 
 import createFluentMarkdownPlugin from '../bld/library';
 
-import './style.css';
-
 const PLUGINS = [createFluentMarkdownPlugin()];
+
+const DEMO_CONTENT = convertFromRaw({
+  blocks: [
+    {
+      key: 'efs48',
+      text: 'Hello, Draft.js',
+      type: 'header-one',
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: 'evlc2',
+      text:
+        'This is a demo for draft-js-fluent-markdown-plugin, have fun typing!',
+      type: 'unstyled',
+      depth: 0,
+      inlineStyleRanges: [
+        {
+          offset: 10,
+          length: 4,
+          style: 'BOLD',
+        },
+        {
+          offset: 19,
+          length: 31,
+          style: 'CODE',
+        },
+      ],
+      entityRanges: [],
+      data: {},
+    },
+  ],
+  entityMap: {},
+});
 
 interface AppProps {}
 
@@ -19,7 +53,7 @@ interface AppState {
 
 class App extends Component<AppProps, AppState> {
   state = {
-    editorState: EditorState.createEmpty(),
+    editorState: EditorState.createWithContent(DEMO_CONTENT),
   };
 
   render(): ReactNode {
